a3275a9281c0796f2c6cf0257773b0010eb8d5a3
Leonel Barrios <leonel.barriosmay@gmail.com>
2025-06-24 20:24:58 -0400

    reporte usuarios

---
+++ src/main/java/com/example/fumi_forte/controllers/ReportesController.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.fumi_forte.controllers;
+
+import com.example.fumi_forte.dto.UsuarioReporteDto;
+import com.example.fumi_forte.services.UsuarioReporteService;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reporte")
+@RequiredArgsConstructor
+public class ReportesController {
+    @Autowired
+    private UsuarioReporteService usuarioService;
+
+    @PostMapping("/usuario")
+    public List<UsuarioReporteDto> buscarUsuarios(
+        @RequestBody Map<String, String> filtros) {
+        String nombre = "%"+filtros.get("nombre")+"%";
+        String estado = "%"+filtros.get("estado")+"%";
+        String rol = "%"+filtros.get("rol")+"%";
+        return usuarioService.buscarUsuarios(nombre, estado, rol);
+    }
+}
---
+++ src/main/java/com/example/fumi_forte/services/UsuarioReporteService.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.fumi_forte.services;
+
+import com.example.fumi_forte.dto.UsuarioReporteDto;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioReporteService {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<UsuarioReporteDto> buscarUsuarios(String nombre, String estado, String rol) {
+        String sql = """
+            SELECT id_usuario, nombre_completo, correo, estado, rol, telefono, direccion
+            FROM usuario
+            WHERE nombre_completo like ? AND estado like ? AND rol like ?
+            ORDER BY nombre_completo desc
+        """;
+
+        return jdbcTemplate.query(sql, new Object[]{nombre, estado, rol}, (rs, rowNum) -> {
+            UsuarioReporteDto usuario = new UsuarioReporteDto();
+            usuario.setIdUsuario(rs.getInt("id_usuario"));
+            usuario.setNombreCompleto(rs.getString("nombre_completo"));
+            usuario.setCorreo(rs.getString("correo"));
+            usuario.setEstado(rs.getString("estado"));
+            usuario.setRol(rs.getString("rol"));
+            usuario.setTelefono(rs.getString("telefono"));
+            usuario.setDireccion(rs.getString("direccion"));
+            return usuario;
+        });
+    }
+
+}
